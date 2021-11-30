5f13c2dd8b7c17d65b5e99d196839475c83baabb
Rezuan Kassim <rezuan@easyparcel.com>
2021-11-30 22:14:01 +0800

    finish layout

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="font-sans">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,55 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      pluviophile: '#66E2DC',
+      'island-pink': '#FA7453',
+      'warm-apricot': '#FFB964',
+      'cool-december': '#FCFAF9',
+      'coarse-wool': '#191826',
+    },
+    fontSize: {
+      h1: [
+        '88px',
+        {
+          lineHeight: '88px',
+          letterSpacing: '-1px',
+        },
+      ],
+      h2: [
+        '40px',
+        {
+          lineHeight: '52px',
+        },
+      ],
+      body: [
+        '20px',
+        {
+          lineHeight: '34px',
+        },
+      ],
+      'body-2': [
+        '16px',
+        {
+          lineHeight: '26px',
+        },
+      ],
+    },
+    fontFamily: {
+      sans: ['IBM Plex Sans'],
+    },
+    extend: {
+      backgroundImage: {
+        mobile: "url('/bg-main-mobile.png')",
+        tablet: "url('/bg-main-tablet.png')",
+        desktop: "url('/bg-main-desktop.png')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
